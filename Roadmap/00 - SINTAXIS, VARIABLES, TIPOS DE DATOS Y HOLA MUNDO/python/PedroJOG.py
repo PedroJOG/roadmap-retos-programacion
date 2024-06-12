50cbdfc6ab69a9bb989b351f8fbe2adbaf0c17cb
PedroJOG <144932073+PedroJOG@users.noreply.github.com>
2024-06-12 12:42:43 +0200

    PedroJOG.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/PedroJOG.py
@@ -0,0 +1,16 @@
+## Python. https://www.python.org/ ##
+
+'''
+Esta es otra manera de representar
+comentarios
+'''
+
+Nombre = Pedro
+
+string = "Esta es una cadena"
+int = 5
+float = 5.5
+bool = True
+complex = 2 + 7j
+
+print("¡Hola, Python")
